4fae53fdf27780d6b084f5f56b02774a0e52f8b1
kelteren <thomas.karlsen@gmail.com>
2012-05-02 00:51:46 +0300

    Added license

--- I2C_master/I2C_master.ino
+++ I2C_master/I2C_master.ino
@@ -1,3 +1,14 @@
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <github.com/kelteren> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Thomas Karlsen
+ * ----------------------------------------------------------------------------
+ */
+
+
 #include <Wire.h>
 
 int ledA = 7;
